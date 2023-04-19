64c14f5d251d282ac503dd738f63503a660ab08a
Tan <tanb2012139@student.ctu.edu.vn>
2023-04-19 12:07:43 +0700

    Update

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "./assets/themify-icons/themify-icons.css"
 import './assets/main.css'
-
+ádf
 createApp(App).mount('#app')
 import router from "./router";
 createApp(App).use(router).mount("#app");
